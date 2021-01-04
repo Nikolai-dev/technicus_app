b2d8de594ec534eb9626216ad8825da93824dfe9
Nikolai <nikolai.muehlbacher@htl-klu.at>
2021-01-04 14:49:50 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp (home: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Icon(Icons.ac_unit,
+            color: Colors.white70,
+            size: 100,
+        ),
+        backgroundColor: Colors.brown[600],
+      ),
+      body: Center(
+        child: Text("Fuck you!"),
+      )
+
+    );
+  }
+}
