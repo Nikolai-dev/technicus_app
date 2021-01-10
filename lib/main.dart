cce6c8b7349386e98e6c8ff2a7b4ae9ae953e339
Jojo-del <johannes.petautschnig@gmx.at>
2021-01-10 15:39:23 +0100

    async http request when tapping on the welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,76 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_technicus/secondPage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
-void main() => runApp(MaterialApp (home: MyApp()));
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        //backgroundColor: Colors.black54,
-        body: FirstPage(),
-      ),
-    );
-  }
-}
-
-class FirstPage extends StatefulWidget {
-  @override
-  _FirstPageState createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 7),
-        (){
-
-      Navigator.push(context, new MaterialPageRoute(builder: (conext)=>NextPage()));
-        }
-
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Stack(
-        children: <Widget>[
-          new Container(
-            decoration: new BoxDecoration(
-              image: new DecorationImage(image: new AssetImage("assets/woods.jpg"), fit: BoxFit.cover,),
+    return GestureDetector(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/woods.jpg"), fit: BoxFit.cover,),
+              ),
             ),
-          ),
-          new Center(
-            child: new Text("What's up world?",
-              style: TextStyle(
-                fontSize: 49,
-                fontWeight: FontWeight.w900,
-                fontFamily: "Lato",
-                color: Colors.white,
-              )
-            )
-          ),
-        ],
+            Center(
+                child: Text("What's up world?",
+                    style: TextStyle(
+                      fontSize: 49,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: "Lato",
+                      color: Colors.white,
+                    )
+                )
+            ),
+          ],
 
+        ),
       ),
+      onTap: () async{
+
+        String loginURL = 'http://192.168.1.121:69/api/login';
+        var response = await http.get(
+            Uri.encodeFull(loginURL), headers: {'key': '6ru6dsh5kD8oHC79a_78segQ5_eR96'});
+        //print('ok');
+        if(response.statusCode == 200){
+          var body= jsonDecode(response.body);
+          print(response.body);
+          if(body['status'] == 'key accepted'){
+
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Menu()));
+          }
+
+        }
+        else{
+          print('scheisse');
+
+        }
+      }
     );
   }
 }
+
+
+
 
 
