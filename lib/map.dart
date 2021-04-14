7af97afff38e53605a19ae0b52c467db27d7cdb5
Nikolai <nikolai.muehlbacher@htl-klu.at>
2021-04-14 21:21:12 +0200

    viel passiert

--- lib/map.dart
+++ lib/map.dart
@@ -23,15 +23,118 @@ class _MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
+    return new Scaffold(
+      appBar: AppBar(
+       // title: Text("Slidemenu"),
+        backgroundColor: Colors.transparent//Colors.grey[800], //Color.fromRGBO(240, 248, 255, 99)//Colors.transparent,//Color.fromRGBO(39, 64, 139, 100)
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          children:<Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: <Color>[
+                    Colors.black,
+                    Colors.grey[800]
+                  ])
+                ),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      elevation:10,
+                      child: Padding(padding: EdgeInsets.all(8.0),
+                          child: Image.asset("assets/Flutter Logo.png", width: 80, height:80),),
+                    ),
+                    Padding(padding: EdgeInsets.all(8.0),child: Text("Earthtastic",style: TextStyle(color: Colors.white, fontSize: 21.0),),)
+                  ],
+                )
+              ),),
+            CustomListTile(Icons.settings, "Settings", ()=>{}),
+            CustomListTile(Icons.fireplace_outlined, "Waldbrände", ()=>{}),
+            CustomListTile(Icons.water_damage, "Tsunami",()=>{}),
+           CustomListTile(Icons.dangerous,"Erdbeben", ()=>{}),
+          ],
+        )
+      ),
+      body:
+      GoogleMap(
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
           target: startPos,
           zoom: 10
       ),
+
       mapType: widget.myMapType,
       polygons: widget.polygonList,
       circles: widget.earthquakeslist,
-     );
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+       // onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+
+      ),
+
+    );
+
+   // return GoogleMap(
+     // onMapCreated: _onMapCreated,
+     // initialCameraPosition: CameraPosition(
+      //    target: startPos,
+    //      zoom: 10
+     // ),
+     // mapType: widget.myMapType,
+     // polygons: widget.polygonList,
+    // circles: widget.earthquakeslist,
+     //);
+  }
+}
+
+class CustomListTile extends StatelessWidget{
+
+  IconData icon;
+  String text;
+  Function onTap;
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(
+            color: Colors.grey.shade400
+          ))
+        ),
+        child: InkWell(
+          splashColor: Colors.grey[800],
+        onTap: onTap,
+        child: Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+             Row(
+               children: <Widget>[
+              Icon(icon),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:  Text(text,style: TextStyle(
+                  fontSize: 16.0,
+              ),
+              )),
+              ],
+              ),
+              Icon(Icons.arrow_drop_down_sharp),
+            ],
+          ),
+        ),
+      ),
+      ),
+    );
   }
 }
