82487ec8c70dd6f36a47571ada3fbcdb1e0ad85c
jp <johannes.p2005@gmail.com>
2021-06-03 14:59:46 +0200

    fix http request, remove stuff

--- lib/main.dart
+++ lib/main.dart
@@ -21,75 +21,6 @@ class App extends StatelessWidget {
 }
 
 
-/*class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        child: Scaffold(
-          body: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/SplashScreen_V2.png"), fit: BoxFit.cover,),
-                ),
-              ),
-            ],
-          ),
-        ),
-
-
-        //http request, triggered by tap on screen
-        onTap: (){
-          Future<http.Response> rsp= login();
-          rsp.then((result){
-            if(result.body == '{"status": "key accepted"}'){
-              print('login done.');
-
-              Future<http.Response> res = request({"Earthquakes": "0"});
-              res.then((resF){
-                print(resF.body);
-
-               // String responseString = '{"place":  "norway",  "mag": 5,  "exampleList": [1, 8, 9, 6]}';
-                //Map<String, dynamic> myData = jsonDecode(resF.body);
-
-                var myData = jsonDecode(resF.body);
-
-               // Set<Circle> earthquakelist;
-
-                //earthquakelist.add
-
-                //print(myData.length);
-                //print(myData[17][6]);
-
-                Set<Circle> earthquakes = {};
-                print(myData.length);
-                for(int i = 0; i < myData.length; i++){
-
-                  earthquakes.add(
-                    Circle(
-                     circleId: CircleId(i.toString()),
-                      radius: myData[i][3]*10000,
-                      center: LatLng(myData[i][1],myData[i][0]),
-                      fillColor: Colors.brown.withOpacity(0.35),
-                      strokeWidth: 1
-                        ,visible: true
-                    ));
-
-                  print(i);
-                }
-
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> Map(myMapType: MapType.hybrid,earthquakeslist: earthquakes,)));
-
-              });
-            }
-          });
-        }
-
-
-    );
-  }
-}*/
-
 class Splashscreen extends StatefulWidget {
   @override
   _SplashscreenState createState() => _SplashscreenState();
@@ -136,17 +67,8 @@ class _SplashscreenState extends State<Splashscreen> {
             res.then((resF){
               print(resF.body);
 
-              // String responseString = '{"place":  "norway",  "mag": 5,  "exampleList": [1, 8, 9, 6]}';
-              //Map<String, dynamic> myData = jsonDecode(resF.body);
+              var myData = jsonDecode("[[-117.6965, 35.8656667, 0.55, 15.5, false], [-97.73245239, 36.71025467, 2.18, 2.138, false]]");
 
-              var myData = jsonDecode(resF.body);
-
-              // Set<Circle> earthquakelist;
-
-              //earthquakelist.add
-
-              //print(myData.length);
-              //print(myData[17][6]);
 
               Set<Circle> earthquakes = {};
               print(myData.length);
@@ -157,7 +79,7 @@ class _SplashscreenState extends State<Splashscreen> {
                         circleId: CircleId(i.toString()),
                         radius: myData[i][3]*10000,
                         center: LatLng(myData[i][1],myData[i][0]),
-                        fillColor: Colors.brown.withOpacity(0.35),
+                        fillColor: Colors.red.withOpacity(0.35),
                         strokeWidth: 1
                         ,visible: true
                     ));
@@ -176,13 +98,13 @@ class _SplashscreenState extends State<Splashscreen> {
 }
 
 Future<http.Response> login() {
-  String loginURL= "http://jojotech.one:3333/api/login";
+  String loginURL= "http://185.119.117.86:8080/api/login";
   Future<http.Response> response= http.get(Uri.encodeFull(loginURL), headers: {'key': '6ru6dsh5kD8oHC79a_78segQ5_eR96'});
   return response;
 }
 
 Future<http.Response> request(var headers){
-  Future<http.Response> response = http.get(Uri.encodeFull("http://jojotech.one:3333/api"), headers: headers);
+  Future<http.Response> response = http.get(Uri.encodeFull("http://185.119.117.86:8080/api"), headers: headers);
 
   return response;
 }
